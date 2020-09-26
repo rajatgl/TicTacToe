5fe0db264a6a2924f6433107d0f0389f881963f7
rajatgl <glrajat@gmail.com>
2020-09-26 10:45:57 +0530

    Showing the Current Board

--- Board.java
+++ Board.java
@@ -1,8 +1,6 @@
 import java.io.*;
 import java.util.*;
-
 public class Board{
-
   private char[] board;
   private final int BOARD_SIZE = 3;
 
@@ -11,13 +9,11 @@ public class Board{
   private char computerChoice;
 
   public Board(){
-
     board = new char[10];
   }
 
   //use case 1
   public void initializeBoard(){
-
     for(int i=0;i<10;i++){
       this.board[i] = '-';
     }
@@ -25,28 +21,22 @@ public class Board{
 
   //use case 2
   public void inputPlayerChoice(){
-
     Scanner sc = new Scanner(System.in);
     char temp = ' ';
-
     while(!isInputValid(temp)){
-
       System.out.print("Please enter your choice, do you choose X or O? ");
       temp = sc.next().charAt(0);
     }
-
-    this.playerChoice = Character.toUpperCase(temp);
-    this.computerChoice = complementChoice(this.playerChoice);
+    this.playerChoice=Character.toUpperCase(temp);
+    this.computerChoice=complementChoice(this.playerChoice);
   }
 
   private boolean isInputValid(char input){
-
-    return Character.toUpperCase(input) == 'X' || Character.toUpperCase(input) == 'O';
+    return Character.toUpperCase(input)=='X'||Character.toUpperCase(input)=='O';
   }
 
   private char complementChoice(char input){
-
-    return input == 'X' ? 'O' : 'X';
+    return input=='X'?'O':'X';
   }
 
   public char getPlayerChoice(){
@@ -57,27 +47,26 @@ public class Board{
     return this.computerChoice;
   }
 
-  public void printBoard(){
-
+  //user case 3
+  public void showBoard(){
     //ignoring 0th element of the board
     int offset = 1;
-
-    for(int i=0;i<BOARD_SIZE;i++){
-
-      for(int j=0;j<BOARD_SIZE;j++){
-          System.out.print(board[i*BOARD_SIZE + j + offset] + " ");
+    System.out.println("\nShowing the Board:\n");
+    for(int i=0;i<3;i++){
+      for(int j=0;j<3;j++){
+          System.out.print(" "+board[i*3+j+offset]+" ");
+          if(j<2) System.out.print("|"); else System.out.print(" ");
       }
 
-      System.out.println();
+      if(i==2) System.out.println(); else System.out.println("\n---+---+---");
     }
   }
 
   public static void main(String[] args){
-
       Board board = new Board();
       board.initializeBoard();
       board.inputPlayerChoice();
-
       System.out.println("\nPlayer Choice: " + board.getPlayerChoice() + " Computer Choice: " + board.getComputerChoice());
+      board.showBoard();
   }
 }
